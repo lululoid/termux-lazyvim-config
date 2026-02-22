c7deb0d26dad74f5b64e7f9e7fc8d9ea54dd99e8
lululoid <nreiwn@gmail.com>
2026-02-22 12:12:39 +0700

    Update: Add personal configuration for termux

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    -- other noice options
+    views = {
+      cmdline_popup = {
+        size = {
+          min_width = 40,          width = "auto",           height = "auto",
+        },
+      },
+    },
+  },
+}
+
